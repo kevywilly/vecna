37c8a25f8ae4fcaa834d3656d9a31080377db718
Kevin Williams <kevywilly@gmail.com>
2024-02-20 19:18:30 -0500

    angles

--- src/vecna.py
+++ src/vecna.py
@@ -8,9 +8,29 @@ class Vecna:
         coxa = 25
         femur = 40
         tibia = 85
+        tibia_z_offset = 85-30
+        
+        angle_0 = 0
+        angle1 = 5
+        angle2 = -90
 
-    joints = np.array([[0,1,2],[4,5,6],[8,9,10],[12,13,14]])
-    dir = np.array([[1,1,1],[1,-1,-1],[1,1,1],[1,-1,-1]])
+    joints = np.array(
+        [
+            [0,1,2],
+            [4,5,6],
+            [8,9,10],
+            [12,13,14]
+        ]
+    )
+    dir = np.array(
+        [
+            [1,1,1],
+            [1,-1,-1],
+            [1,1,1],
+            [1,-1,-1]
+        ]
+    )
+
     adj = np.array(
         [
             [0,0,5],
